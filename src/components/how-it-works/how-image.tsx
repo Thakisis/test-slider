55c287ad60033bb7f95ada17910dd8cad8f96138
Thakisis <sub722@gmail.com>
2026-01-04 15:43:45 +0100

    added slider

---
+++ src/components/how-it-works/how-image.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Image from "next/image";
+import { type Step } from "./how-it-works";
+
+function HowImage({
+	image,
+	title,
+	priority,
+	number,
+}: Step & { priority: boolean }) {
+	return (
+		<div
+			className="relative transition-opacity duration-700 ease-in-out aspect-video lg:aspect-square rounded-2xl overflow-hidden shadow-2xl border border-slate-200 col-start-2 [.active]:opacity-100 opacity-0"
+			style={{
+				gridColumn: 2,
+				gridRow: "1 / -1",
+				gridArea: "1 / 2 / -1 / 3", // Forzar explícitamente el área
+			}}
+			data-step={number}
+		>
+			<Image
+				src={image || "/placeholder.svg"}
+				alt={title}
+				fill
+				className="absolute inset-0 object-contain p-4 w-full h-full"
+				priority={priority}
+			/>
+		</div>
+	);
+}
+
+export default HowImage;
---
+++ src/components/how-it-works/how-it-works.tsx
@@ -0,0 +1,63 @@
+import HowHeader from "./how-header";
+import HowWrapper from "./how-wrapper";
+import HowButton from "./how-button";
+import HowImage from "./how-image";
+export interface Step {
+	number: number;
+	title: string;
+	description: string;
+	image: string;
+}
+const STEPS: Step[] = [
+	{
+		number: 1,
+		title: "Sube tus datos",
+		description:
+			"Simplemente cargue sus datos a nuestra plataforma segura. Admitimos diversos formatos de archivo y tipos de datos para garantizar una integración perfecta con sus sistemas existentes.",
+		image: "/images/step-1.jpg", // added image references for each step
+	},
+	{
+		number: 2,
+		title: "Haga clic en Inicio",
+		description:
+			"Nuestros algoritmos de IA avanzados procesan y analizan automáticamente sus datos, extrayendo información valiosa y patrones que serían difíciles de identificar manualmente.",
+		image: "/images/step-2.jpg",
+	},
+	{
+		number: 3,
+		title: "Obtenga información útil",
+		description:
+			"Reciba información y recomendaciones claras y prácticas basadas en el análisis de IA. Utilice esta información para tomar decisiones basadas en datos y mejorar sus estrategias comerciales.",
+		image: "/images/step-3.jpg",
+	},
+	{
+		number: 4,
+		title: "Obtenga información útil",
+		description:
+			"Reciba información y recomendaciones claras y prácticas basadas en el análisis de IA. Utilice esta información para tomar decisiones basadas en datos y mejorar sus estrategias comerciales.",
+		image: "/images/step-4.jpg",
+	},
+];
+
+export default function HowItWorks() {
+	const stepsButtons = STEPS.map((step) => (
+		<HowButton key={`desktop-${step.number}`} {...step} />
+	));
+	const stepsImages = STEPS.map((step, index) => (
+		<HowImage key={`desktop-${step.number}`} {...step} priority={index === 0} />
+	));
+
+	return (
+		<section className="w-[70%] mx-auto border-b border-border flex flex-col justify-center items-center">
+			<HowHeader />
+
+			<div className="w-full px-4 py-3 md:px-8 w-full ">
+				<HowWrapper stepsCount={STEPS.length}>
+					{stepsButtons}
+
+					{stepsImages}
+				</HowWrapper>
+			</div>
+		</section>
+	);
+}
