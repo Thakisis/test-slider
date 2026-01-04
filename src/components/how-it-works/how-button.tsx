8feb551f2b8acd05484a09f5bf119cdb74129094
Thakisis <sub722@gmail.com>
2026-01-05 00:38:00 +0100

    fix: removed unused import

--- src/components/how-it-works/how-button.tsx
+++ src/components/how-it-works/how-button.tsx
@@ -1,33 +1,32 @@
-import React from "react";
-import { type Step } from "./how-it-works";
+import type { Step } from "./how-it-works";
 
 function HowButton({ number, title, description }: Step) {
-	return (
-		<button
-			type="button"
-			className="group relative flex text-left gap-6 transition-opacity duration-300 "
-			style={{
-				gridColumn: 1,
-				gridRow: number,
-			}}
-			data-step={number}
-		>
-			{/* Left indicator line with progress */}
-			<div className="absolute -left-4 top-0 h-full w-1 bg-slate-100 rounded-full overflow-hidden ">
-				<div className="w-full bg-primary transition-all duration-100 ease-linear group-[.active]:animate-progress-bar" />
-			</div>
+  return (
+    <button
+      type="button"
+      className="group relative flex text-left gap-6 transition-opacity duration-300 "
+      style={{
+        gridColumn: 1,
+        gridRow: number,
+      }}
+      data-step={number}
+    >
+      {/* Left indicator line with progress */}
+      <div className="absolute -left-4 top-0 h-full w-1 bg-slate-100 rounded-full overflow-hidden ">
+        <div className="w-full bg-primary transition-all duration-100 ease-linear group-[.active]:animate-progress-bar" />
+      </div>
 
-			{/* Content */}
-			<div className="flex flex-col gap-2 py-1">
-				<h3 className="text-xl font-bold text-slate-900">
-					{number}. {title}
-				</h3>
-				<p className="max-w-md text-slate-600 leading-relaxed text-sm md:text-base">
-					{description}
-				</p>
-			</div>
-		</button>
-	);
+      {/* Content */}
+      <div className="flex flex-col gap-2 py-1">
+        <h3 className="text-xl font-bold text-slate-900">
+          {number}. {title}
+        </h3>
+        <p className="max-w-md text-slate-600 leading-relaxed text-sm md:text-base">
+          {description}
+        </p>
+      </div>
+    </button>
+  );
 }
 
 export default HowButton;
